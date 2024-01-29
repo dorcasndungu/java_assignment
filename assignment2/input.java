5a73b1d389a93167bda0063bfa0c7a95b88e65ef
dorcasndungu <ndungumwihaki017@gmail.com>
2024-01-29 23:18:41 +0300

    adds scanner

---
+++ assignment2/input.java
@@ -0,0 +1,10 @@
+package assignment2;
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        
+    }
+    
+}
